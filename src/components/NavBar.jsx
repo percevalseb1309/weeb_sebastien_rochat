f41a679ee8ab23bf57b4e21337a95778146ffd27
Percevalseb <percevalseb@gmail.com>
2026-07-13 15:45:22 +0200

    feat: setup React Router DOM

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">weeb</Link>
+                </li>
+                <li>
+                    <a href="">About us</a>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/login">Log In</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Layout from "./Layout.jsx";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home/>,
+            },
+            {
+                path: "contact",
+                element: <Contact/>,
+            },
+            {
+                path: "login",
+                element: <Login/>,
+            },
+        ],
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
