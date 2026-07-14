0166659c3b22a15b6d7cfd1badcafa2608b58c9b
Percevalseb <percevalseb@gmail.com>
2026-07-14 16:29:28 +0200

    feat: create Hero component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Resources from "../components/Resources";
 
 function Home() {
   return (
     <div>
-        <NavBar/>
         <Hero/>
         <Resources/>
-        <Footer/>
     </div>
   );
 }
